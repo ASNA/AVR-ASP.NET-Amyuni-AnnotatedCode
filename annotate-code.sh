f56097330463f439dff74334a70dd1c3aa489cf5
Roger Pence <roger.pence@gmail.com>
2015-12-11 09:11:07 -0600

    Refresh annotated code

--- annotate-code.sh
+++ annotate-code.sh
@@ -1,6 +1,6 @@
-pycco ../TestAmyuniDriver/app_code/*.vr -p -d code -l javascript
-pycco ../TestAmyuniDriver/*.vr -p -d code -l javascript
-pycco ../../Other/ASNA.AmyUniPDF/ASNA.AmyUniPDF/*.vr -p -d code/ASNA.AmyuniPDF -l javascript
+pycco ../TestAmyuniDriver/app_code/*.vr -d code/TestAmyuniDriver/app_code -l javascript
+pycco ../TestAmyuniDriver/*.vr          -d code/TestAmyuniDriver          -l javascript
+pycco ../../Other/ASNA.AmyUniPDF/ASNA.AmyUniPDF/*.vr -d code/ASNA.AmyuniPDF -l javascript
 
 git add -A
 git commit -m 'Refresh annotated code'
