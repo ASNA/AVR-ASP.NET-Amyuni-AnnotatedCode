51484a3628adc8e0e90e7c2c8db49a9a20035b3f
Roger Pence <roger.pence@gmail.com>
2015-12-11 08:48:21 -0600

    Refresh annotated code

---
+++ annotate-code.sh
@@ -0,0 +1,5 @@
+pycco ../TestAmyuniDriver/app_code/*.vr -p -d code -l javascript
+pycco ../TestAmyuniDriver/*.vr -p -d code -l javascript
+git add -A
+git commit -m 'Refresh annotated code'
+git push
